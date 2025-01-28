ac5535cf7c28fdcff76e1c5884c2a59615a7c687
Arthur Ish <arthurishdev@gmail.com>
2025-01-27 22:23:35 -0800

    passed fetch plant array and added loop logic to generate plant cards

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,21 @@
 import React from "react";
 
-function PlantCard() {
+function PlantCard({ plant }) {
+  // Add logic to check if plant is in stock and render appropriate button
+  const inStock = true;
+  function handleClick() {
+    inStock = !inStock;
+  }
+
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+      {inStock ? (
+        <button className="primary" onClick={handleClick}>In Stock</button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleClick}>Out of Stock</button>
       )}
     </li>
   );
