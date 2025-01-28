43e5ebbecf443fb8bb6c16bf8cff1bf4c2187174
Arthur Ish <arthurishdev@gmail.com>
2025-01-27 22:14:23 -0800

    initial fetch added

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+//fetch plants, add state?
+const [plants, setPlants] = useState([]);
+useEffect(() => {
+  fetch("http://localhost:6001/plants")
+  .then((r) => r.json())
+  .then((data) => setPlants(data))
+  //console.log("ran")
+}, []);
+
+
+
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList plants={plants}/>
     </main>
   );
 }
